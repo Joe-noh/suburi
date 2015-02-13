0fa99f0542a0111dee98bc9a1985f3c0e5455653
Joe-noh <goflb.jh@gmail.com>
2015-02-13 17:43:13 +0900

    toggle all between complete/incomplete

--- todo_app/app/controllers/todos.js
+++ todo_app/app/controllers/todos.js
@@ -23,6 +23,16 @@ export default Ember.ArrayController.extend({
     }
   },
 
+  allAreDone: function (key, value) {
+    if (value === undefined) {
+      return (this.get('length') !== 0) && this.isEvery('isCompleted');
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted'),
+
   remaining: function () {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
